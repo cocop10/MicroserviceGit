e15671da339595878cc1cc6b0a27869c57281fa4
cocop10 <66115379+cocop10@users.noreply.github.com>
2021-01-28 17:35:32 +0100

    Modif html/sql

--- MicroServices-master/product/src/main/resources/data.sql
+++ MicroServices-master/product/src/main/resources/data.sql
@@ -1,10 +1,10 @@
 INSERT INTO PRODUCT(id, name , description, illustration, price) values
-(0, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(1, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(2, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(3, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(4, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(5, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(6, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(7, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150),
-(8, 'Nom de produit', 'Description qui donne envie d''acheter', 'https://picsum.photos/300/300', 150);
+(0, 'Product-0', 'Priscis est fides ad explanare ulla fides nobilem explanare nunc maioribus locuta maioribus ut hoc fama est enim quae ut pertinacior inde.', 'https://picsum.photos/id/1/200/300', 150),
+(1, 'Product-1', 'Priscis est fides ad explanare ulla fides nobilem explanare nunc maioribus locuta maioribus ut hoc fama est enim quae ut pertinacior inde.', 'https://picsum.photos/id/2/200/300', 120),
+(2, 'Product-2', 'Priscis est fides ad explanare ulla fides nobilem explanare nunc maioribus locuta maioribus ut hoc fama est enim quae ut pertinacior inde.', 'https://picsum.photos/id/3/200/300', 10),
+(3, 'Product-3', 'Priscis est fides ad explanare ulla fides nobilem explanare nunc maioribus locuta maioribus ut hoc fama est enim quae ut pertinacior inde.', 'https://picsum.photos/id/4/200/300', 99),
+(4, 'Product-4', 'Priscis est fides ad explanare ulla fides nobilem explanare nunc maioribus locuta maioribus ut hoc fama est enim quae ut pertinacior inde.', 'https://picsum.photos/id/5/200/300', 5),
+(5, 'Product-5', 'Priscis est fides ad explanare ulla fides nobilem explanare nunc maioribus locuta maioribus ut hoc fama est enim quae ut pertinacior inde.', 'https://picsum.photos/id/6/200/300', 15),
+(6, 'Product-6', 'Priscis est fides ad explanare ulla fides nobilem explanare nunc maioribus locuta maioribus ut hoc fama est enim quae ut pertinacior inde.', 'https://picsum.photos/id/7/200/300', 20),
+(7, 'Product-7', 'Priscis est fides ad explanare ulla fides nobilem explanare nunc maioribus locuta maioribus ut hoc fama est enim quae ut pertinacior inde.', 'https://picsum.photos/id/8/200/300', 70),
+(8, 'Product-8', 'Priscis est fides ad explanare ulla fides nobilem explanare nunc maioribus locuta maioribus ut hoc fama est enim quae ut pertinacior inde.', 'https://picsum.photos/id/9/200/300', 40);
